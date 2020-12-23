54196e62387f873a5b13467f82361660dbb7cea7
“KrisReid” <krisreid_06@hotmail.com>
2020-12-23 22:13:29 +0000

    Initial Commit

---
+++ BookSwap/BookSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSwapApp.swift
+//  BookSwap
+//
+//  Created by Kris Reid on 23/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct BookSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
